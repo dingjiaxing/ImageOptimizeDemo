4cf9b8f716c8a64dc214d727f831068f3ecbd6d5
dingjiaxing <v_jiaxing.ding@pactera.com>
2019-08-30 14:07:09 +0800

    fix bug

--- app/src/main/java/com/enjoy/leo_bitmapcache/MainActivity.java
+++ app/src/main/java/com/enjoy/leo_bitmapcache/MainActivity.java
@@ -3,11 +3,12 @@ package com.enjoy.leo_bitmapcache;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
